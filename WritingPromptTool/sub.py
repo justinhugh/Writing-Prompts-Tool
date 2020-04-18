e69f085b45b00d2de54f9a211e01756212d3aa42
Justin Hugh <justin.hugh@gmail.com>
2020-04-18 16:16:35 -0400

    Combined the three explicit file read functions into one agnostic. Print formatting. While loop to repeat execution of main function.

--- WritingPromptTool/sub.py
+++ WritingPromptTool/sub.py
@@ -1,21 +1,10 @@
 import tkinter.filedialog
-
-# Test function to test import of this file.
-def Test():
-    print("This is the test function.")
+import os
 
 # Function to write introductory text at start of the program.
 def Intro():
-    print('\n\nThis is a program that will provide you with a character, a conflict, and a setting to write about.\n\n')
-
-# Function to get and open files
-def Get_all_files():
-    print('\n\nYou will be prompted for 3 file locations. Please select the location of character file, then conflicts file, then settings file.\n\n')
-    input('press enter to begin...')
-    characters_filename = tkinter.filedialog.askopenfilename()
-    conflicts_filename = tkinter.filedialog.askopenfilename()
-    settings_filename = tkinter.filedialog.askopenfilename()
-    return characters_filename, conflicts_filename, settings_filename
+    print('\n\nThis is a program that will provide you with a character, a '
+          'conflict, and a setting to write about.')
 
 # Function to print text and ask for the prompt you're looking for.
 def Request_prompt_type():
@@ -31,7 +20,8 @@ def Request_prompt_type():
 
     # Request input from user until valid response is entered.
     while user_input not in acceptable:
-        user_input = input('Type one of "character", "conflict", "setting", or "all"\n>>>: ')
+        user_input = input('\n\nType one of "character", "conflict", "setting", or'
+                           ' "all"\n>>>: ')
     return user_input
 
 # Print the prompt type the user entered.
@@ -42,71 +32,67 @@ def Print_prompt_selection(selection):
     if selection == 'all':
         print('\n\nYou asked for a random character, conflict, and setting')
     else:
-        print('\n\nYou asked for a random ', selection,'\n\n',sep='')
+        print('\n\nYou asked for a random ', selection,sep='')
 
-# Function to read character text file, and pull a list of all characters
-def Read_characters(open_file):
-    '''(file open for reading) -> list of str()
+def Read_from_file(requested_prompt):
+    '''(str) -> list of str()
 
-    Read and return list of characters in characters textfile.
+    Return list of entries in textfile determined by requested_prompt.
 
-    Precondition: characters textfile is a list of characters, one per line,
-    with no blank lines.
-    '''
-    # Initialize empty list to accumulate characters.
-    character_list = []
-    # Read and store first character in first line.
-    line = open_file.readline().strip('\n')
-
-    # Until we reach an empty line, append the list and read next line,
-    # storing it.
-    while line !='':
-        character_list.append(line)
-        line = open_file.readline().strip('\n')
-    return character_list
-
-# Function to read conflicts text file, and pull a list of all conflicts
-def Read_conflicts(open_file):
-    '''(file open for reading) -> list of str()
-
-    Read and return list of conflicts in conflicts textfile.
-
-    Precondition: conflicts textfile is a list of conflicts, one per line,
-    with no blank lines.
-    '''
-    # Initialize empty list to accumulate settings.
-    conflicts_list = []
-    # Read and store first character in first line.
-    line = open_file.readline().strip('\n')
-
-    # Until we reach an empty line, append the list and read next line,
-    # storing it.
-    while line !='':
-        conflicts_list.append(line)
-        line = open_file.readline().strip('\n')
-    return conflicts_list
-
-# Function to read settings text file, and pull a list of all settings
-def Read_settings(open_file):
-    '''(file open for reading) -> list of str()
-
-    Read and return list of settings in settings textfile.
-
-    Precondition: settings textfile is a list of settings, one per line,
+    Precondition: textfile is a list of entries, one per line,
     with no blank lines.
     '''
 
     # Initialize empty list to accumulate settings.
-    settings_list = []
-    # Read and store first character in first line.
-    line = open_file.readline().strip('\n')
+    list = []
+
+    # Find the correct file(s) to read based on requested_prompt. Use a switch.
+    __location__ = os.path.realpath(
+                                    os.path.join(os.getcwd(),
+                                    os.path.dirname(requested_prompt + 's.text'))
+                                    )
+    f = open(os.path.join(__location__, requested_prompt + 's.text'),'r');
+
+    line = f.readline().strip('\n')
 
     # Until we reach an empty line, append the list and read next line,
     # storing it.
     while line !='':
-        settings_list.append(line)
-        line = open_file.readline().strip('\n')
-    return settings_list
+        list.append(line)
+        line = f.readline().strip('\n')
+    f.close()
+    return list
 
-def Read_all(open_file_characters, open_file_conflicts, open_file_settings):
-    pass
+def Read_all():
+    '''
+    Return list of list of entries in each of the prompt list textfiles
+
+    Precondition: Each textfile is a list of entries, one per line,
+    with no blank lines.
+    '''
+    # Initialize empty list to accumulate settings.
+    list_of_lists = {}
+    all_list = ['character', 'conflict', 'setting']
+
+    for each_type in all_list:
+        list = []
+        # Find the correct file(s) to read based on requested_prompt. Use a switch.
+        __location__ = os.path.realpath(
+                                        os.path.join(os.getcwd(),
+                                        os.path.dirname(each_type + 's.text'))
+                                        )
+        f = open(os.path.join(__location__, each_type + 's.text'),'r');
+
+        line = f.readline().strip('\n')
+
+        # Until we reach an empty line, append the list and read next line,
+        # storing it.
+        while line !='':
+            list.append(line)
+            line = f.readline().strip('\n')
+        f.close()
+        list_of_lists[each_type] = list
+    return list_of_lists
+
+def Thanks_for_using():
+    print("\n\nOkay, thanks for using Justin's Writing Prompt Tool!\n\n")
--- WritingPromptTool/writing_prompt.py
+++ WritingPromptTool/writing_prompt.py
@@ -3,39 +3,44 @@ import sub
 import random
 import tkinter.filedialog
 
-
-filenames = sub.Get_all_files()
-open_file_characters = open(filenames[0],'r')
-open_file_conflicts = open(filenames[1],'r')
-open_file_settings = open(filenames[2],'r')
+# Initiate boolean, storing whether program should restart.
+continue_var = True
 
 # Print text to introduce user to purpose of program.
 sub.Intro()
 
-# Get the user's input for the type of prompt they're looking for.
-requested_prompt = sub.Request_prompt_type()
+while continue_var == True:
+    # Get the user's input for the type of prompt they're looking for.
+    requested_prompt = sub.Request_prompt_type()
 
-# Print confirmation of selection
-sub.Print_prompt_selection(requested_prompt)
+    # Print confirmation of selection
+    sub.Print_prompt_selection(requested_prompt)
 
-# Read the corresponding text file, get list of strings
-# Initialize a switch statement, which gives the correct function to use.
+    # Read the corresponding text file, get list of strings
+    # Initialize a switch statement, which gives the correct function to use.
 
-switch_prompt_function ={
-                    "character": sub.Read_characters(open_file_characters),
-                    "conflict": sub.Read_conflicts(open_file_conflicts),
-                    "setting": sub.Read_settings(open_file_settings),
-                    "all": sub.Read_all(open_file_characters,open_file_conflicts,open_file_settings),
-                    }
+    # Create the appropriate list of prompts
 
-# Create the appropriate list of prompts
-prompt_list = switch_prompt_function.get(requested_prompt)
+    # If single type, run Read_from_file function.
+    if requested_prompt != 'all':
+        prompt_list = sub.Read_from_file(requested_prompt)
+        print('\nHere is your random', requested_prompt, ':\n')
+        # Return random string from list
+        print(prompt_list[random.randrange(0,len(prompt_list))])
+    else:
+        prompt_list = sub.Read_all()
+        for each in prompt_list:
+            print('\nHere is your random', each, ':\n')
+            # Return random string from list
+            print(prompt_list[each][random.randrange(0,len(prompt_list[each]))])
 
-# Print random entry from list
-print(prompt_list[random.randrange(0,len(prompt_list))])
+    # Return to start of program
+    continue_response = ''
 
-# Print random prompt_list
+    while continue_response not in ('yes', 'no'):
+        continue_response = input('\nWould you like another prompt? enter "yes"'
+                                  'or "no"\n>>>:')
 
-# Return random string from list
-
-# Print corresponding prompt(s)
+    if continue_response == 'no':
+        continue_var = False
+        sub.Thanks_for_using()
